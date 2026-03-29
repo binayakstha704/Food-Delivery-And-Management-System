cf4e0c22dc23fdb9dd65d86e81faab29447cc047
maverick9797 <maverickthapa111@gmail.com>
2026-03-29 23:39:55 +0545

    Initial upload of User Registration files

--- db.php
+++ db.php
@@ -2,11 +2,22 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
+<<<<<<< HEAD
 $dbname = "Swaad_Unlimited";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+=======
+$db   = "Swaad_Unlimited";
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database Connection Failed: " . $conn->connect_error);
+>>>>>>> 735e22a (Updating user page)
 }
 ?>
